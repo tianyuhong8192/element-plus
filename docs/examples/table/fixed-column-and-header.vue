6143341c3586b734f567caded240e179fe84b6c0
tianyuhong8192 <37405075+tianyuhong8192@users.noreply.github.com>
2022-06-27 10:18:12 +0800

    Update fixed-column-and-header.vue

--- docs/examples/table/fixed-column-and-header.vue
+++ docs/examples/table/fixed-column-and-header.vue
@@ -1,7 +1,7 @@
 <template>
   <el-table :data="tableData" style="width: 100%" height="250">
     <el-table-column fixed prop="date" label="Date" width="150" />
-    <el-table-column prop="name" label="Name" width="120" />
+    <el-table-column fixed prop="name" label="Name" width="120" />
     <el-table-column prop="state" label="State" width="120" />
     <el-table-column prop="city" label="City" width="320" />
     <el-table-column prop="address" label="Address" width="600" />
